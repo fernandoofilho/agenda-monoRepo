a1c5f120fb73ab0e362594d8fa60df283fa948ed
vs0808 <vinicius.souza.menezes@gmail.com>
2024-10-09 19:05:01 -0300

    header docs

--- projectAddressBook/src/app/contact.service.ts
+++ projectAddressBook/src/app/contact.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
+  private apiUrl = 'https://sua-api.com/contacts'; // URL da sua API
+
+  constructor(private http: HttpClient) { }
+
+  // Método para buscar contatos
+  getContacts(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  // Método para adicionar um novo contato
+  addContact(contact: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    return this.http.post(this.apiUrl, contact, { headers });
+  }
 }
