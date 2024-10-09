ea60cc9e8ed1caec70851af6b2aeacba51322b7f
Maylon Martins de Castro <109696916+mayloncastro@users.noreply.github.com>
2024-10-09 16:29:46 -0400

    listage-item

---
+++ api/src/models/contactModel.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const getAllContacts = async () => {
+  return prisma.person.findMany();
+};
+
+const createContact = async (data: {
+  name: string;
+  email: string;
+  surname: string;
+  phone: string;
+  image: string;
+}) => {
+  return prisma.person.create({ data });
+};
+
+const deleteContact = async (id: number) => {
+  return await prisma.person.delete({
+    where: {
+      id: id,
+    },
+  });
+};
+
+const updateContact = async (id: number, data: { name?: string, email?: string, surname?: string, phone?: string, image?: string }) => {
+  return await prisma.person.update({
+    where: { id: id },
+    data: data,
+  });
+};
+
+export default {
+  getAllContacts,
+  createContact,
+  deleteContact,
+  updateContact
+};
